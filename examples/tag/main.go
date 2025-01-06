73601dea8cfc676cbfe090ba00f3441c67db6eae
Masayoshi Mizutani <masayoshi.mizutani@dr-ubie.com>
2025-01-06 09:57:19 +0900

    add example of tag

---
+++ examples/tag/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/m-mizutani/goerr"
+)
+
+var (
+	ErrTagSysError   = goerr.NewTag("system_error")
+	ErrTagBadRequest = goerr.NewTag("bad_request")
+)
+
+func handleError(w http.ResponseWriter, err error) {
+	if goErr := goerr.Unwrap(err); goErr != nil {
+		switch {
+		case goErr.HasTag(ErrTagSysError):
+			w.WriteHeader(http.StatusInternalServerError)
+		case goErr.HasTag(ErrTagBadRequest):
+			w.WriteHeader(http.StatusBadRequest)
+		default:
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	} else {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	_, _ = w.Write([]byte(err.Error()))
+}
+
+func someAction() error {
+	if _, err := http.Get("http://example.com/some/resource"); err != nil {
+		return goerr.Wrap(err, "failed to get some resource").WithTags(ErrTagSysError)
+	}
+	return nil
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if err := someAction(); err != nil {
+			handleError(w, err)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("OK"))
+	})
+
+	http.ListenAndServe(":8090", nil)
+}
